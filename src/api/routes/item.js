50a5bc4c30bf50ffb55f8801c11f9cef1710e6aa
jag2501 <jag6613@gmail.com>
2021-03-15 13:30:00 +0100

    items export to csv

--- src/api/routes/item.js
+++ src/api/routes/item.js
@@ -7,6 +7,25 @@ const route = express.Router();
 // All operations require to be logged
 route.use('/', middlewares.checkValidUser);
 
+route.get('/export', async (req, res) => {
+  const result = await itemService.exportcsv({
+    ref: req.query['referencia'],
+    fam: req.query['familia'],
+  });
+
+  if (result === "nonExistent") {
+    res.status(401).send("non existent family");
+    return;
+  }
+  if (result === "error") {
+    res.status(500).send("error");
+    return;
+  }
+
+  res.attachment('data.csv');
+  res.status(200).send(result);
+});
+
 route.get('/:id?', async (req, res) => {
   const result = await itemService.read({
     id: req.params['id'],
--- src/services/item.js
+++ src/services/item.js
@@ -9,13 +9,22 @@ class ItemService {
       let query;
       const params = [];
 
-      query = "SELECT * FROM ARTICULO WHERE TELEMATICO=1";
+      const querySelectTratamientoAlmacen = "SELECT EXISTENCIAS_TRATAMIENTO, EXISTENCIAS_ALMACEN, VENDER_SIN_EXISTENCIAS FROM TIENDA_VIRTUAL WHERE ID=1";
+      let dataTratamientoAlmacen = await FirebirdPromise.aquery(querySelectTratamientoAlmacen, [], "comun");
+      const tratamiento = dataTratamientoAlmacen[0].EXISTENCIAS_TRATAMIENTO;
+      const idAlmacen = dataTratamientoAlmacen[0].EXISTENCIAS_ALMACEN;
+
+      if(tratamiento){ //Se vende de un almacén en concreto
+        query = "SELECT A.*, F.DESCRIPCION AS NOMBRE_FAMILIA, (SELECT SUM(E.EXISTENCIAS) FROM EXISTENCIA E WHERE E.ID_ARTICULO = A.ID_ARTICULO AND E.ID_ALMACEN=?) AS STOCK FROM ARTICULO A INNER JOIN FAMILIA F ON A.ID_FAMILIA = F.ID_FAMILIA WHERE A.TELEMATICO=1";
+      }else{ //Se vende globalmente sin tener en cuenta el almacén
+        query = "SELECT A.*, F.DESCRIPCION AS NOMBRE_FAMILIA, (SELECT SUM(E.EXISTENCIAS) FROM EXISTENCIA E WHERE E.ID_ARTICULO = A.ID_ARTICULO) AS STOCK FROM ARTICULO A INNER JOIN FAMILIA F ON A.ID_FAMILIA = F.ID_FAMILIA WHERE A.TELEMATICO=1";
+      }
       if (id) {
-        query += " AND ID_ARTICULO=?";
+        query += " AND A.ID_ARTICULO=?";
         params.push(id);
       }
       if (ref) {
-        query += " AND REFERENCIA=?";
+        query += " AND A.REFERENCIA=?";
         params.push(ref);
       }
       if (fam) {
@@ -24,19 +33,110 @@ class ItemService {
         let dataFamilia = await FirebirdPromise.aquery(querySelectFamilia, paramsSelectFamilia, "empresa");
         if(dataFamilia[0]){
           const idFamilia = dataFamilia[0].ID_FAMILIA;
-          query += " AND ID_FAMILIA=?";
+          query += " AND A.ID_FAMILIA=?";
           params.push(idFamilia);
         }else{
           return 'nonExistent';
         }
       }
 
-      query += " ORDER BY LOWER(DESCRIPCION)";
+      query += " ORDER BY LOWER(A.DESCRIPCION)";
       
       let data = await FirebirdPromise.aquery(query, params, "empresa");
       return data;
     } catch (error) {
       console.log("Error at ItemService -> read: "+error);
+      return 'error';
+    }
+  }
+
+  async exportcsv({ ref, fam }) {
+    try {
+      let query;
+      const params = [];
+
+      const querySelectTratamientoAlmacen = "SELECT EXISTENCIAS_TRATAMIENTO, EXISTENCIAS_ALMACEN, VENDER_SIN_EXISTENCIAS FROM TIENDA_VIRTUAL WHERE ID=1";
+      let dataTratamientoAlmacen = await FirebirdPromise.aquery(querySelectTratamientoAlmacen, [], "comun");
+      const tratamiento = dataTratamientoAlmacen[0].EXISTENCIAS_TRATAMIENTO;
+      const idAlmacen = dataTratamientoAlmacen[0].EXISTENCIAS_ALMACEN;
+
+      if(tratamiento){ //Se vende de un almacén en concreto
+        query = "SELECT A.*, F.DESCRIPCION AS NOMBRE_FAMILIA, (SELECT SUM(E.EXISTENCIAS) FROM EXISTENCIA E WHERE E.ID_ARTICULO = A.ID_ARTICULO AND E.ID_ALMACEN=?) AS STOCK FROM ARTICULO A INNER JOIN FAMILIA F ON A.ID_FAMILIA = F.ID_FAMILIA WHERE A.TELEMATICO=1";
+      }else{ //Se vende globalmente sin tener en cuenta el almacén
+        query = "SELECT A.*, F.DESCRIPCION AS NOMBRE_FAMILIA, (SELECT SUM(E.EXISTENCIAS) FROM EXISTENCIA E WHERE E.ID_ARTICULO = A.ID_ARTICULO) AS STOCK FROM ARTICULO A INNER JOIN FAMILIA F ON A.ID_FAMILIA = F.ID_FAMILIA WHERE A.TELEMATICO=1";
+      }
+      if (ref) {
+        query += " AND A.REFERENCIA=?";
+        params.push(ref);
+      }
+      if (fam) {
+        const querySelectFamilia = "SELECT ID_FAMILIA FROM FAMILIA WHERE DESCRIPCION=?";
+        const paramsSelectFamilia = [fam];
+        let dataFamilia = await FirebirdPromise.aquery(querySelectFamilia, paramsSelectFamilia, "empresa");
+        if(dataFamilia[0]){
+          const idFamilia = dataFamilia[0].ID_FAMILIA;
+          query += " AND A.ID_FAMILIA=?";
+          params.push(idFamilia);
+        }else{
+          return 'nonExistent';
+        }
+      }
+
+      query += " ORDER BY LOWER(A.DESCRIPCION)";
+      
+      let data = await FirebirdPromise.aquery(query, params, "empresa");
+      const { Parser } = require('json2csv')
+      const fields = [
+        {
+          label: 'ID',
+          value: 'ID_ARTICULO'
+        },
+        {
+          label: 'sku',
+          value: 'REFERENCIA'
+        },
+        {
+          label: 'post_title',
+          value: 'DESCRIPCION'
+        },
+        {
+          label: 'post_content',
+          value: 'DESCRIPCION'
+        },
+        {
+          label: 'regular_price',
+          value: 'PRECIO_VENTA_TIENDA_VIRTUAL'
+        },
+        {
+          label: 'sale_price',
+          value: 'PRECIO_VENTA_TIENDA_VIRTUAL'
+        },
+        {
+          label: 'stock',
+          value: 'STOCK'
+        },
+        {
+          label: 'weight',
+          value: 'TAMANO'
+        },
+        {
+          label: 'Images',
+          value: 'IMAGEN'
+        },
+        {
+          label: 'tax:product_cat',
+          value: 'NOMBRE_FAMILIA'
+        },
+        {
+          label: 'meta:attribute_color',
+          value: 'COLOR'
+        }
+      ];
+      const json2csv = new Parser({ fields: fields });
+      const csv = json2csv.parse(data);
+      return csv;
+    } catch (error) {
+      console.log("Error at ItemService -> exportcsv: "+error);
       return 'error';
     }
   }
